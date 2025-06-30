146c891547e456c430cf276a99c7938c0b4676ea
Zach <86675944+zwgnr@users.noreply.github.com>
2025-06-30 16:22:15 -0400

    fix searchable select

--- packages/components/src/examples/select-searchable.tsx
+++ packages/components/src/examples/select-searchable.tsx
@@ -2,15 +2,17 @@ import { SearchableSelect, SelectItem } from "../core/select/select";
 
 export const SearchableSelectExample = () => {
 	return (
-		<SearchableSelect 
-			label="Model"
-			searchPlaceholder="Search"
-      className="w-96"
-		>
-			<SelectItem>4o</SelectItem>
-			<SelectItem>o3</SelectItem>
-			<SelectItem>Sonnet 4</SelectItem>
-			<SelectItem>Gemini 2.0</SelectItem>
-		</SearchableSelect>
+		<div className="flex w-full flex-col gap-2">
+			<SearchableSelect
+				label="Model"
+				searchPlaceholder="Search"
+				className="w-48"
+			>
+				<SelectItem textValue="4o">4o</SelectItem>
+				<SelectItem textValue="o3">o3</SelectItem>
+				<SelectItem textValue="Sonnet 4">Sonnet 4</SelectItem>
+				<SelectItem textValue="Gemini 2.0">Gemini 2.0</SelectItem>
+			</SearchableSelect>
+		</div>
 	);
-}; 
+};
